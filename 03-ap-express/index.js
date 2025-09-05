ba8255c1fad7e0a4e68e2ca945cfa88abde99716
aleesousa <aleepsousa010@gmail.com>
2025-09-04 21:47:28 -0300

    Inicio Projeto Calculadora-express

---
+++ 03-ap-express/index.js
@@ -0,0 +1,67 @@
+// importar o express
+
+const express = require("express");
+
+// crio uma instância () da minha aplicação
+
+const app = express();
+
+// guardar o numero da porta que vai ser alocada
+
+const porta = 3000;
+
+// Middlewares (intermediários)
+// Intermediários de Log
+app.use((req, res, next) => {
+  console.log("Time: ", new Date().toLocaleString());
+  console.log("Metodo: ", req.method);
+  console.log("Rota: ", req.url);
+  next();
+});
+
+app.get("/nome", (req, res, next) => {
+//capturar informações dos usuários
+// vao vir atraves dos parametros da requisição (query params) É enviado pelo POSTMAN
+  const primeiroNome = req.query.primeiroNome;
+  const sobreNome = req.query.sobreNome;
+
+  res.send("Olá " + primeiroNome + " " + sobreNome + "!");
+});
+
+//importando o router de calculadora de nota
+const calculadoraNotaRouter = require('./routes/calculadoraNota')
+// Toda requisição que chegar na rota /calculadora vai paara o router
+app.use('/calculadora', calculadoraNotaRouter)
+
+// Método e a rota
+// Req -> Dados da requisição
+// Res -> Manipulador da resposta
+// Next -> Chamador do proximo middleware
+app.get("/teste", (req, res, next) => {
+  res.send("TESTE ATUALIZAR");
+});
+
+app.get("/pessoas", (req, res, next) => {
+  const pessoas = [
+    {
+      id: 1,
+      nome: "Bu",
+      idade: "20",
+    },
+    {
+      id: 2,
+      nome: "ceta",
+      idade: "30",
+    },
+  ];
+
+  res.json(pessoas);
+});
+
+// Executa a aplicação escolhendo a porta
+app.listen(porta, () => {
+  // Imprimo uma mensagem para confirmar que aplicação está funcionando (rodando na porta escolhida)
+  console.log("Aplicação rodando em http://localhost:3000");
+});
+
+// Executa a aplicação escolhendo
---
+++ 03-ap-express/routes/calculadoraNota.js
@@ -0,0 +1,33 @@
+// Importar o express
+const express = require("express");
+
+// criar um router
+const router = express.Router();
+
+//Mapeamento das rotas e implemento a lógica de cada
+router.get("/notaA1", (req, res, next) => {
+  const exercicio = parseFloat(req.query.exercicio);
+  const trabalho = parseFloat(req.query.trabalho);
+  const prova = parseFloat(req.query.prova);
+
+// Validar se os parametros existem
+    if(isNaN(exercicio) || isNaN(trabalho) || isNaN(prova)){
+        return res.status(400).json({ erro: "Notas Inválidas!!!" })
+}
+
+//valido se as notas estao no intervalo correto
+    if(exercicio < 0 || exercicio > 1 || trabalho < 0 || trabalho > 3 || prova < 0 || prova > 6){
+        return res.status(400).json({ erro: "Notas fora do intervalo" })
+}
+
+  const notaA1 = exercicio + trabalho + prova;
+
+  res.json({ notaA1 });
+});
+
+//Calcula a nota do A2
+
+
+//Calcula a média final (A1 40% & A2 60%)
+
+module.exports = router;
