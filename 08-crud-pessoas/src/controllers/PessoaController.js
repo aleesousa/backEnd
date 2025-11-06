79a45e459609e8af593aeb37d4e3b7abfb3c4db6
aleesousa <aleepsousa010@gmail.com>
2025-11-06 20:27:53 -0300

    API Gerenciamento

--- 08-crud-pessoas/src/controllers/PessoaController.js
+++ 08-crud-pessoas/src/controllers/PessoaController.js
@@ -1,21 +1,20 @@
 const express = require("express");
 const router = express.Router;
-const PessoaModel = require('../models/PessoaModel')
+const PessoaModel = require("../models/PessoaModel");
+const { PessoaValidator } = require("../validators/PessoaValidator");
 
 // Rotas do CRUD
 // Create
-router.post('/pessoas', async (req, res, next) => {
-    const pessoa = req.body
-    const pessoaCadastrada = await PessoaModel.create(pessoa)
-    res.status(201).json(pessoaCadastrada)
-})
+router.post("/pessoas", validarNovaPessoa async (req, res, next) => {
+  const pessoa = req.body;
+  const pessoaCadastrada = await PessoaModel.create(pessoa);
+  res.status(201).json(pessoaCadastrada);
+});
 
 // Read
 
 // Update
 
 //Delete
-
-
 
 module.exports = router;
